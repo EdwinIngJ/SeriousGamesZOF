94257a81e69e2e3ed015ca742c8113f068c571f9
Ethan Yen <ethyn7603@gmail.com>
2020-07-22 10:30:29 -0400

    Added more labels

--- GUI.py
+++ GUI.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import font
 
 HEIGHT = 620
 WIDTH  = 1250
@@ -12,6 +11,15 @@ frame = tk.Frame(root, bg = "#263D42", bd = 5) #hex teal blue
 frame.place(relx = 0.5, rely= 0.01, relwidth= 1, relheight= 0.97, anchor = 'n')
 
 label = tk.Label(frame, text = '"Deployment lablels go here"', font = ('Courier', 15))
-label.place(relwidth= 0.33, relheight= 0.8)
+label.place(relwidth= 0.25, relheight= 0.95)
+
+TitleCard = tk.Label(frame, text = "Zombies or Flu", font = ('Courier', 18))
+TitleCard.place(relx = 0.5, rely = 0.001, relwidth = 0.49, relheight  = 0.1, anchor = 'n')
+
+Turn = tk.Label(frame, text = 'Turn: ', font = ('Courier', 12))
+Turn.place(relx = 0.75, rely = 0.001, relwidth = 0.1, relheight = 0.05)
+
+Score = tk.Label(frame, text = 'Score: ', font = ('Courier', 12))
+Score.place(relx = 0.86, rely = 0.001, relwidth = 0.1, relheight = 0.05)
 
 root.mainloop()
