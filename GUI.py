8a11ab042728489d99751909fc86c22f177c5b18
Ethan Yen <ethyn7603@gmail.com>
2020-07-23 10:42:40 -0400

    Added some buttons for deployments

--- GUI.py
+++ GUI.py
@@ -10,9 +10,6 @@ canvas.pack()
 frame = tk.Frame(root, bg = "#263D42", bd = 5) #hex teal blue
 frame.place(relx = 0.5, rely= 0.01, relwidth= 1, relheight= 0.97, anchor = 'n')
 
-label = tk.Label(frame, text = '"Deployment lablels go here"', font = ('Courier', 15))
-label.place(relwidth= 0.25, relheight= 0.95) #placeholder for the list of buttons with deployments
-
 TitleCard = tk.Label(frame, text = "Zombies or Flu", font = ('Courier', 18))
 TitleCard.place(relx = 0.5, rely = 0.001, relwidth = 0.49, relheight  = 0.1, anchor = 'n') #header with game title
 
@@ -27,6 +24,7 @@ Fear.place(relx =0.75, rely = 0.055, relwidth = 0.204, relheight = 0.05) #Label 
 
 CityFrame = tk.Frame(root, bg = 'gray', bd = 5)
 CityFrame.place(relx = 0.255, rely = 0.13, relwidth = 0.725, relheight = 0.83) #Frame to hold all 9 City labels
+
 ### The 9 Neighborhoods ###
 NWest = tk.Label(CityFrame, text = 'hi', bd = 5)
 NWest.place(relwidth = 0.33, relheight = 0.33)
@@ -54,6 +52,47 @@ South.place(relx = 0.335, rely = 0.67, relwidth = 0.33, relheight = 0.33)
 
 SEast = tk.Label(CityFrame, text = 'hi', bd = 5)
 SEast.place(relx = 0.669, rely = 0.67, relwidth = 0.33, relheight = 0.33)
+
+DeployFrame = tk.Label(frame, bg = 'gray', bd = 5)
+DeployFrame.place(relwidth= 0.25, relheight= 0.99) #frame to hold the 25 Deployment buttons
+
+###The 25 Buttons ###
+NoDeploy = tk.Button(DeployFrame, text = "No Depoloyment", font = ('Courier', 7))
+NoDeploy.place(relwidth = 1)
+
+QuarantineOpen = tk.Button(DeployFrame, text = "Quarantine Open", font = ('Courier', 7))
+QuarantineOpen.place(rely = 0.04, relwidth = 1)
+
+QuarantineFenced = tk.Button(DeployFrame, text = "Quarantine Fenced", font = ('Courier', 7))
+QuarantineFenced.place(rely = 0.08, relwidth = 1)
+
+BiteCenterDisinfect = tk.Button(DeployFrame, text = "Bite Center Disinfect", font = ('Courier', 7))
+BiteCenterDisinfect.place(rely = 0.12, relwidth = 1)
+
+BiteCenterAmputate = tk.Button(DeployFrame, text = "Bite Center Amputate", font = ('Courier', 7))
+BiteCenterAmputate.place(rely = 0.16, relwidth = 1)
+
+ZCureFDA = tk.Button(DeployFrame, text = "Z Cure (FDA)", font = ('Courier', 7))
+ZCureFDA.place(rely = 0.2, relwidth = 1)
+
+ZCureEXP = tk.Button(DeployFrame, text = "Z Cure (EXP)", font = ('Courier', 7))
+ZCureEXP.place(rely = 0.24, relwidth = 1)
+
+FluVaccOpt = tk.Button(DeployFrame, text = "Flu Vaccine (Optional)", font = ('Courier', 7))
+FluVaccOpt.place(rely = 0.28, relwidth = 1)
+
+FluVaccMan = tk.Button(DeployFrame, text = "Flu Vaccine (Mandatory)", font = ('Courier', 7))
+FluVaccMan.place(rely = 0.32, relwidth = 1)
+
+KilnOVersight = tk.Button(DeployFrame, text = "Kiln (Oversight)", font = ('Courier', 7))
+KilnOVersight.place(rely = 0.36, relwidth = 1)
+
+KilnNoQ = tk.Button(DeployFrame, text = "Kiln (No Questions)", font = ('Courier', 7))
+KilnNoQ.place(rely = 0.4, relwidth = 1)
+
+BroadcastDontPanic = tk.Button(DeployFrame, text = "Broadcast Dont Panic", font = ('Courier', 7))
+BroadcastDontPanic.place(rely = 0.44, relwidth = 1)
+
 
 
 root.mainloop()
