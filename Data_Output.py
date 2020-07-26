65fb5c49d4a703a51eb266722d5dce643c67f0b7
Arib Chowdhury <aribc@mit.edu>
2020-07-26 14:33:01 -0400

    Scatterplot between standard deviation of local fears and final score complete. Couldn't figure out how to do a curve of best fit though

--- Data_Output.py
+++ Data_Output.py
@@ -1,32 +1,33 @@
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
-#from gym_zgame.envs.ZGameEnv import decode_raw_action
+import statistics as stat
+import numpy as np
 from gym_zgame.envs.enums.PLAYER_ACTIONS import DEPLOYMENTS, LOCATIONS
-#from gym_zgame.envs.model.City import City
 
 
 class Data_Output:
     def __init__(self, data_log):
-        self.data_to_output = pd.DataFrame(pd.read_json(data_log, lines=True))[-140:]
-        self.data_npc_count_to_score = {'total_score' : [],
-                                        'fear' : [],
-                                        'num_alive' : [],
-                                        'num_dead' : [],
-                                        'num_ashen' : [],
-                                        'num_human' : [],
-                                        'num_zombie_bitten' : [],
-                                        'num_zombie' : [],
-                                        'num_healthy' : [],
-                                        'num_incubating' : [],
-                                        'num_flu' : [],
-                                        'num_immune' : [],
-                                        'num_moving' : [],
-                                        'num_moving' : [],
-                                        'num_active' : [],
-                                        'num_sickly' : [],
-                                        'original_dead' : [],
-                                        'original_alive' : []
+        self.data_to_output = pd.DataFrame(pd.read_json(data_log, lines=True))[-1400:]
+        self.data_npc_count_to_score = {
+            'total_score' : [],
+            'fear' : [],
+            'num_alive' : [],
+            'num_dead' : [],
+            'num_ashen' : [],
+            'num_human' : [],
+            'num_zombie_bitten' : [],
+            'num_zombie' : [],
+            'num_healthy' : [],
+            'num_incubating' : [],
+            'num_flu' : [],
+            'num_immune' : [],
+            'num_moving' : [],
+            'num_moving' : [],
+            'num_active' : [],
+            'num_sickly' : [],
+            'original_alive' : [],
+            'original_dead' : []
         }
         self.data_local_fear_to_score = {
             #Local Fears
@@ -68,20 +69,26 @@ class Data_Output:
                 break
         return int(output)
 
-    def cutRead(self, string, cuts, keys):
-        for i in range(len(cuts)):
-            string = self.cut(string, cuts[i])
-            self.data_npc_count_to_score[keys[i]].append(self.readNumber(string))
+    def cutRead(self, string, cuts1, keys1, cuts2, keys2):
+        for i in range(len(cuts1)):
+            string = self.cut(string, cuts1[i])
+            self.data_npc_count_to_score[keys1[i]].append(self.readNumber(string))
+        for i in range(len(cuts2)):
+            string = self.cut(string, cuts2[i])
+            self.data_local_fear_to_score[keys2[i]].append(self.readNumber(string))
             
     def parseRawState(self):
         for index in self.data_to_output['step'].keys():
             if self.data_to_output['step'][index] == 13:
                 self.game_count += 1
                 string = self.data_to_output['raw_state'][index]
-                keys = list(self.data_npc_count_to_score.keys())
-                cuts = [x + '\": ' for x in keys]
-                self.cutRead(string, cuts, keys)           
-            
+                keys1 = list(self.data_npc_count_to_score.keys())
+                cuts1 = [x + '\": ' for x in keys1]
+                keys2 = list(self.data_local_fear_to_score.keys())
+                cuts2 = ['local_fear\": ' for _ in range(9)]
+                self.cutRead(string, cuts1, keys1, cuts2, keys2)
+        self.data_local_fear_to_score['total_score'] = self.data_npc_count_to_score['total_score'][:]
+        
     def deployment_usage_graph(self):
         all_actions = self.data_to_output['actions']
         num_deps_used = {}
@@ -112,15 +119,23 @@ class Data_Output:
         plt.plot(x_axis, self.data_npc_count_to_score['num_incubating'], color = "olive", label = "Number of Incubating")
         plt.plot(x_axis, self.data_npc_count_to_score['num_flu'], color = "pink", label = "Number of Flu")
         plt.xlabel("Game Number")
-        plt.ylabel("Amount of NPC Type Left Standing")
-        plr.title("NPC Counts and Global Fear Compared to Score")
+        plt.ylabel("Amount")
+        plt.title("NPC Counts and Global Fear Compared to Score")
         plt.xticks(rotation = 90)
         plt.tight_layout()
         plt.legend()
         plt.show()
 
-    #def local_fear_std_to_score_graph(self):
-        
+    def local_fear_std_to_score_graph(self):
+        keys = list(self.data_local_fear_to_score.keys())
+        x_axis = [stat.stdev([self.data_local_fear_to_score[key][i] for key in keys]) for i in range(len(self.data_local_fear_to_score["NW"]))]
+        plt.scatter(x_axis, self.data_local_fear_to_score['total_score'])
+        plt.xlabel("Standard Deviation between Local Fears")
+        plt.ylabel("Final Score")
+        plt.title("Effect of Local Fear STD on Final Score")
+        plt.xticks(rotation = 90)
+        plt.tight_layout()
+        plt.show()
         
         
 if __name__ == '__main__':
@@ -128,4 +143,4 @@ if __name__ == '__main__':
     data = Data_Output(data_log)
     data.deployment_usage_graph()
     data.npc_count_to_score_graph()
-    #data.local_fear_std_to_score_graph()
+    data.local_fear_std_to_score_graph()
